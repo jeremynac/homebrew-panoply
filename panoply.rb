6c496ca04124d50616e90d3dd955b7a280c4debb
jeremynac <jeremy.naccache@epitech.eu>
2021-12-12 10:22:58 +0000

    Update from https://github.com/jeremynac/panoply/commit/f93bcddaa78d8aac72b64955b1562062107b11dd

--- panoply.rb
+++ panoply.rb
@@ -3,10 +3,10 @@ class Panoply < Formula
 
   desc "Save your commands and reuse them"
   homepage "https://github.com/jeremynac/panoply"
-  url "https://files.pythonhosted.org/packages/d0/59/8b3dfab969eb5f74135d9ade90a1d987993b588e944990d8737737cee62e/panoply-0.1.44.tar.gz"
-  sha256 "827d38f7ddaf6f18920ecd551937fd90ee1ddeafeda6dc440a0cf6d69ba46ab2"
+  url "https://files.pythonhosted.org/packages/cd/0e/0f1941eddb73b39c969f5ea17891593e83b3877f0a67049a2ffdb0fd6736/panoply-0.1.45.tar.gz"
+  sha256 "d8f5d2241da0294e47fdbde12ef140b4c87599b752d30e187ed977712919cafc"
 
-  depends_on 'python@3.9'
+  depends_on "python3"
 
   resource "simple-term-menu" do
     url "https://files.pythonhosted.org/packages/de/08/08c19f7653a9d7c73df024090243a8a44583b613f8f1ed51d79cbb5ae70e/simple-term-menu-1.4.1.tar.gz"
@@ -14,19 +14,11 @@ class Panoply < Formula
   end
 
   def install
-    virtualenv_create(libexec, "python@3.9")
+    virtualenv_create(libexec, "python3")
     virtualenv_install_with_resources
   end
 
   test do
-    assert_match "There are no commands saved yet\n\
-    usage: panoply [-h] [-a] [-r]\n\
-    \n\
-    Save your commands and reuse them\
-    \n\
-    optional arguments:\n\
-      -h, --help    show this help message and exit\n\
-      -a, --add     add a command\n\
-      -r, --remove  remove a command\n", shell_output(bin/"panoply")
+    false
   end
 end
